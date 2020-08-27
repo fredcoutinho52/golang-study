5ee792c5ad4ed29c40129cca09de43e5eb1a19c3
Fred Coutinho <42944398+fredcoutinho52@users.noreply.github.com>
2020-08-27 17:38:08 -0300

    For loop

---
+++ control/for/for.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	for i := 0; i <= 20; i += 2 {
+		fmt.Println(i)
+	}
+
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println("Even")
+		} else {
+			fmt.Println("Odd")
+		}
+	}
+
+	// infinite loop
+	for {
+		fmt.Println("Forever...")
+		time.Sleep(time.Second)
+	}
+}
+
+// IMPORTANT
+// Golang don't have while loop
